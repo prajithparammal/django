b0c21828e63e97a0a69172d1520dfac378a96b96
Prajith Parammal <prajithparammal@gmail.com>
2019-10-08 16:48:40 +0800

    updated models.py, admin.py and setting.py

--- djangotry/settings.py
+++ djangotry/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # OWN app
+    'products',
 ]
+
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
